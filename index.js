fcf2e4cc67c6cb384e3703874227f82f4beaa76e
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-04-08 12:47:30 +0300

    complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+// const currentUser = 'Grace Hopper'
+// // const welcomeMessage = 'Welcome to Flatbook, ';
+// // const welcomeMessage = currentUser;
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+// typeof "const currentUser = 'Grace Hopper';";
+// "const currentUser = 'Grace Hopper';".length;
+// currentUser;
+// const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
